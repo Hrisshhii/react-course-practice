580929aac42db3b02bd45a522266d6e8e7827845
Hrishi <rathodhrishi12345@gmail.com>
2026-01-08 21:58:13 +0530

    Seperate into smaller components

---
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { OrderSummary } from './OrderSummary';
+import {useState,useEffect} from 'react';
+import { PaymentSummary } from './PaymentSummary';
+import './checkout-header.css';
+import './CheckoutPage.css';
+import { CheckoutHeader } from './CheckoutHeader';
+
+export function CheckoutPage({cart}){
+  const [deliveryOptions,setDeliveryOptions]=useState([]);
+  const [paymentSummary,setPaymentSummary]=useState(null);
+  useEffect(()=>{
+    axios.get('/api/delivery-options?expand=estimatedDeliveryTime').then((response)=>{setDeliveryOptions(response.data)});
+    axios.get('/api/payment-summary').then((response)=>{setPaymentSummary(response.data)});
+  },[]);
+
+  return (
+  <>
+    <title>Checkout</title>
+
+    <CheckoutHeader />
+
+    <div className="checkout-page">
+      <div className="page-title">Review your order</div>
+
+      <div className="checkout-grid">
+        <OrderSummary cart={cart} deliveryOptions={deliveryOptions}/>
+        <PaymentSummary paymentSummary={paymentSummary}/>
+      </div>
+    </div>
+  </>
+  );
+}
---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import {useEffect,useState} from 'react';
+import { Header } from '../../components/Header';
+import { ProductsGrid } from './ProductsGrid';
+import './HomePage.css';
+
+export function HomePage({cart}) {
+  const [products,setProducts]=useState([]);
+  
+  // has two values name of data and updater function
+  useEffect(()=>{
+    axios.get('/api/products').then((response)=>{setProducts(response.data);});
+    
+  },[]);
+  // [] is a dependency arry and lets us control when useEffect runs 
+  // - [] empty means only once
+  return (
+    <>
+      <title>Ecommerce Project</title>
+
+      <Header cart={cart}/>
+
+      <div className="home-page">
+        <ProductsGrid products={products}/>
+      </div>
+    </>
+  );
+}
