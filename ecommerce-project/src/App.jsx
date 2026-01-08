c286e397bdada8ca921ddb9539ce9286a9d03aeb
Hrishi <rathodhrishi12345@gmail.com>
2026-01-08 23:19:00 +0530

    Indentation fix

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -9,23 +9,23 @@ import { TrackingPage } from './pages/tracking/TrackingPage'
 import './App.css'
 
 function App() {
-    const [cart,setCart]=useState([]);
-    useEffect(()=>{
-        const fetchAppData= async ()=>{
-            const response= await axios.get('/api/cart-items?expand=product')
-            setCart(response.data);
-        };
-        fetchAppData();
-    },[]);
-    
-    return(
-        <Routes>
-            <Route index element={<HomePage cart={cart}/>} />
-            <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
-            <Route path="orders" element={<OrdersPage cart={cart}/>} />
-            <Route path="tracking" element={<TrackingPage />} />
-        </Routes>
-    );
+  const [cart,setCart]=useState([]);
+  useEffect(()=>{
+    const fetchAppData= async ()=>{
+      const response= await axios.get('/api/cart-items?expand=product')
+        setCart(response.data);
+      };
+      fetchAppData();
+  },[]);
+  
+  return(
+    <Routes>
+      <Route index element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
+      <Route path="orders" element={<OrdersPage cart={cart}/>} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+  );
 }
 
 export default App
