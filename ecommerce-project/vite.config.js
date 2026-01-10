435752e40446f31a1277b07ff5c9b87fb437e0f5
Hrishi <rathodhrishi12345@gmail.com>
2026-01-10 19:05:44 +0530

    Create new Typescript project

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
   server:{
     proxy:{
       '/api':{
@@ -13,8 +17,8 @@ export default defineConfig({
         target:'http://localhost:3000'
       }
     }
-  },
-  base: '/react-course-practice/ecommerce-project/'
+  }
+  //base: '/react-course-practice/ecommerce-project/'
   /*
     build:{
       outDir:'../ecommerce-backend/dist'
